3e8c4e6430f9454ad7591a6522b87feea426e5c9
samy- <d87.jason@gmail.com>
2016-03-11 12:05:49 +0100

    Adding server src files

---
+++ server/consts.h
@@ -0,0 +1,36 @@
+﻿#ifndef CONSTS
+#define CONSTS
+
+#include <QString>
+
+// Server
+//=======Network========
+const QString _serverToken("[Server]");
+const QString _instantMessage("instantMessage");
+const QString _newMessage("newMessage");
+const QString _savedMessage("savedMessage");
+const QString _serverOk("ok");
+const QString _serverError("Error");
+// server commands
+const QString _usersConnected("addUser");
+const QString _usersDIsconnected("rmUser");
+//=======Data========
+
+// Client
+//=======Network========
+const QString _clientToken("[Client]");
+const QString _connect("Connect");
+const QString _newAccount("Register");
+const QString _sendMessage("SendMessage");
+const QString _ReceivedMessages("ReceivedMessages");
+const QString _SentMessages("SentMessages");
+const QString _deleteMessage("deleteMessage");
+
+const QString _tcpSeparator("<TCPSeparator>");
+//=======Data========
+const QString _textSeparator("<DataSeparator>");
+const QString _newLine("\n");
+
+const QString _DateTimeFormat("dd/MM/yyyy hh:mm:ss");
+
+#endif  // CONSTS
---
+++ server/main.cpp
@@ -0,0 +1,14 @@
+﻿#include <QApplication>
+#include "serverwindow.h"
+
+int main(int argc, char* argv[])
+{
+    QApplication app(argc, argv);
+
+    // TODO: Seperate the graphical interface from the data
+    // Server must Be a singleton
+    ServerWindow fenetre;
+    fenetre.show();
+
+    return app.exec();
+}
